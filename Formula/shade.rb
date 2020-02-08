319870bfc5a344cd07705db1d45de8baa72224dc
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-02-08 11:08:58 -0600

    Update Shade to 1.0 Beta

--- Formula/shade.rb
+++ Formula/shade.rb
@@ -1,8 +1,8 @@
 class Shade < Formula
     desc "CLI Tools for Phillips Hue Lights"
     homepage "https://Shade.lighting"
-    url "https://github.com/InkApplications/Shade/releases/download/1.0.0-alpha.2/shade.zip"
-    sha256 "73ce1baad3f8de47a2672c47acbe0b86730b3b60d096c4dbe87bcbacf4cbd1d9"
+    url "https://github.com/InkApplications/Shade/releases/download/1.0.0-beta/shade.zip"
+    sha256 "6167f9ed41ce2598fe82c4e5a77baf289e5ccabb32f714df920094e7783c3d7a"
 
     depends_on :java => "1.8"
     bottle :unneeded
